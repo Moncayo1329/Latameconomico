8d11f7b0b96153309bddad0569163e47b962f951
Moncayo1329 <moncayomichael23@gmail.com>
2025-10-22 17:03:15 +0100

    menu done

---
+++ src/menu/menu.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Menu(){
+return (
+    <div>
+        <nav className=" p-3">
+            <ul className="nav justify-content-center">
+                <li className="nav-item">
+                <a href="#" className="nav-link text-black">Economía</a>
+                </li>
+        
+             <li className="nav-item">
+          <a href="#" className="nav-link text-black">Opinión</a>
+        </li>
+        <li className="nav-item">
+          <a href="#" className="nav-link text-black">Nosotros</a>
+</li>
+          </ul>
+        </nav>
+    </div>
+);
+
+}
+
+
+export default Menu;
