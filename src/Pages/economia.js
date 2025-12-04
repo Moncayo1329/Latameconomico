5ef5e539870ff1dc395fdb0cbc354e40505fd309
Moncayo1329 <moncayomichael23@gmail.com>
2025-12-03 20:30:38 -0500

    fix:mardownpath to economics page

--- src/Pages/economia.js
+++ src/Pages/economia.js
@@ -13,12 +13,12 @@ function Economia() {
       const loadedPosts = await Promise.all(
         Economianews.map(async(post)=> {
 
-          const url = require(`../data/economiaData${post.file}`);
+          const url = require(`../posts/economia/${post.markdownPath}`);
           const res = await fetch(url);
           const text = await res.text();
 
-        return {
-            slug: post.slug,
+       return {
+            ...post,
             content: text,
           };
         })
@@ -40,6 +40,7 @@ function Economia() {
       {posts.map((p) => (
         <div key={p.slug}>
           <h2>{p.slug}</h2>
+          <p>{p.description}</p>
           <ReactMarkdown>{p.content}</ReactMarkdown>
         </div>
       ))}
